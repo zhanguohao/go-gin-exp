49549aa17ef1107bb3b5cc315f8b19a70d6b53e2
zhanguohao <zhishengqianjun@163.com>
2020-08-27 10:12:50 +0800

    分离路由

--- main.go
+++ main.go
@@ -5,12 +5,12 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-
 	"github.com/zhanguohao/go-gin-exp/pkg/setting"
+	"github.com/zhanguohao/go-gin-exp/routers"
 )
 
 func main() {
-	router := gin.Default()
+	router := routers.InitRouter()
 	router.GET("/test", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "test",
